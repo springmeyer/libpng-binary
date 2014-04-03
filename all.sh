182ad11ae3f3eff06c4792310c88409f90091a71
Dane Springmeyer <dane@dbsgeo.com>
2014-04-02 21:47:52 -0700

    try building on osx

--- all.sh
+++ all.sh
@@ -1,11 +1,10 @@
-git checkout master ;
 remote=origin ;
-for TRAVIS_BRANCH in `
+for BRANCH in `
     git branch -r | grep $remote | grep -v master | grep -v HEAD | awk '{gsub(/^[^\/]+\//,"",$1); print $1}'
 `; do
-    echo $TRAVIS_BRANCH ;
-    git checkout $TRAVIS_BRANCH ;
+    echo $BRANCH ;
+    git checkout $BRANCH ;
     source ./build.sh ;
-    $TRAVIS_BRANCH ;
+    $BRANCH ;
 done ;
-git checkout master
+git checkout $TRAVIS_BRANCH
