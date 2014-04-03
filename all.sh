af1da052aefe84016a6f26b574813080eaeca962
Dane Springmeyer <dane@dbsgeo.com>
2014-04-02 21:48:53 -0700

    don't recursively build master or osx

--- all.sh
+++ all.sh
@@ -2,9 +2,11 @@ remote=origin ;
 for BRANCH in `
     git branch -r | grep $remote | grep -v master | grep -v HEAD | awk '{gsub(/^[^\/]+\//,"",$1); print $1}'
 `; do
-    echo $BRANCH ;
-    git checkout $BRANCH ;
-    source ./build.sh ;
-    $BRANCH ;
+    if [[ BRANCH != "osx" ]] && [[ BRANCH != "master" ]]; then
+        echo $BRANCH ;
+        git checkout $BRANCH ;
+        source ./build.sh ;
+        $BRANCH ;
+    fi
 done ;
 git checkout $TRAVIS_BRANCH
