9ae9ded6d6364e0e00f6d1b7e73bbf217e55003d
Dane Springmeyer <dane@dbsgeo.com>
2014-04-02 21:52:21 -0700

    fix build

--- all.sh
+++ all.sh
@@ -2,11 +2,11 @@ remote=origin ;
 for BRANCH in `
     git branch -r | grep $remote | grep -v master | grep -v HEAD | awk '{gsub(/^[^\/]+\//,"",$1); print $1}'
 `; do
-    if [[ BRANCH != "osx" ]] && [[ BRANCH != "master" ]]; then
+    if [[ $BRANCH != "osx" ]] && [[ $BRANCH != "master" ]]; then
         echo $BRANCH ;
         git checkout $BRANCH ;
         source ./build.sh ;
-        $BRANCH ;
+        build $BRANCH ;
     fi
 done ;
 git checkout $TRAVIS_BRANCH
